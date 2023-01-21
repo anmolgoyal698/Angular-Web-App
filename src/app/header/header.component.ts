b5e7fc7d727bc3882e03b036a33f611f27294a6f
anmolgoyal698 <anmolgoyal698@gmail.com>
2023-01-21 14:48:40 +0530

    made navbar responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  collapsed = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onWindowResize(event: any) {
+    console.log(event);
+    console.log('Window resized');
+    // this.collapsed = true;
   }
-
 }
